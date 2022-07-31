3b10207f6962ee23031f65303279efabfe393908
Rajini <rajini.parimala@gmail.com>
2022-07-31 19:07:31 +0530

    added 3 ml model code

--- ml_model.py
+++ ml_model.py
@@ -2,3 +2,21 @@
 
 
 print("adding all ML related code here")
+
+
+
+del model1():
+    print("adding code for ML model1")
+
+
+
+
+del model2()):
+    print("adding code for ML model1")
+   
+
+
+
+del model3():
+    print("adding code for ML model1")
+      
